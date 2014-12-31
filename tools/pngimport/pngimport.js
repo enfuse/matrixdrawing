410ed118c9e6417b8b6d8d6579a47003ec166e31
juanjo <juanjo.lopez@biko2.com>
2014-12-31 20:53:07 +0100

    PngImporter now check if image is compatible before import

--- tools/pngimport/pngimport.js
+++ tools/pngimport/pngimport.js
@@ -13,69 +13,76 @@ var jsonPath = 'json/';
 
 function processFiles(err, files){	
 	for(i=0;i<files.length;i++){
-		console.log("Procesando " + files[i]);		
 		convertToJson(files[i]);
 	}
 }
 
 function convertToJson(fileName){
-	easyimg.info(imagesPath + fileName).then(
+	/*easyimg.info(imagesPath + fileName).then(
 	  function(file) {
 	    if(file.width != 16 || file.height != 16 || file.type != 'PNG' || file.depth != '8'){
 	    	easyimg.resize({
 			     src:imagesPath + fileName, dst: imagesPath + fileName + ".PNG",
 			     width:16, height:16
-			  });
+			  }, function(){});
 	    }
 	  }, function (err) {
 	    console.log(err);
 	  }
-	);
+	);*/
 
-	/*
-	var out = {};	
-	fs.createReadStream(imagesPath + fileName)
-	  .pipe(new PNG({
-	      filterType: 4
-	  }))
-	  .on('parsed', function() {
+  easyimg.info(imagesPath + fileName).then(
+    function(fileData) {
+      console.log("Procesando " + fileName);
+      //console.log(file);
+      if(fileData.width == 16 && fileData.height == 16 && fileData.type == 'PNG' && fileData.depth == '8' && fileData.density == '72'){
+        var out = {};
+        fs.createReadStream(imagesPath + fileName)
+          .pipe(new PNG({
+            filterType: 4
+          }))
+          .on('parsed', function() {
 
-	    for (var y = 0; y < this.height; y++) {
-	        for (var x = 0; x < this.width; x++) {
-	            var idx = (this.width * y + x) << 2;
-	            var color = C.RGB(this.data[idx,] this.data[idx+1], this.data[idx+2]);	
-	            var hex = color.hex();
-				out[x+":"+y] = hex;
-	        }
-	    }
-	    
-	    var baseName = fileName.replace(/\.[^/.]+$/, "");
-		var file = jsonPath + baseName + '.json';
+            for (var y = 0; y < this.height; y++) {
+              for (var x = 0; x < this.width; x++) {
+                var idx = (this.width * y + x) << 2;
+                var color = C.RGB(this.data[idx], this.data[idx+1], this.data[idx+2]);
+                var hex = color.hex();
+                out[x+":"+y] = hex;
+              }
+            }
 
-		var artworks = fbref.child("artworks");
-		var child = artworks.child(baseName);
+            var baseName = fileName.replace(/\.[^/.]+$/, "");
+            var file = jsonPath + baseName + '.json';
 
-		child.set( out , function(error) {
-		  if (error) {
-		    console.log("Data could not be saved." + error);
-		  } else {
-		    console.log("Data saved successfully.");
-		  }
-		});
+            var artworks = fbref.child("artworks");
+            var child = artworks.child(baseName);
 
+            child.set( out , function(error) {
+              if (error) {
+                console.log("Data could not be saved." + error);
+              } else {
+                console.log("Data saved successfully.");
+              }
+            });
 
+            jf.writeFile(file, out, function(err) {
+              if(String(null) === "null"){
+                console.log(imagesPath + fileName + " convertido a " + file);
+              }else{
+                console.log(err)
+              }
+            });
+          });
 
+      }else{
+        console.log(fileName + " no es compatible.");
+      }
+    }, function (err) {
+      console.log(err);
+    }
+  );
 
-		jf.writeFile(file, out, function(err) {
-		  if(String(null) === "null"){
-		  	console.log(imagesPath + fileName + " convertido a " + file);
-		  }else{
-		  	console.log(err)
-		  }
-		  	
-		})
-
-	});*/
 }
 
 function resize(file){
